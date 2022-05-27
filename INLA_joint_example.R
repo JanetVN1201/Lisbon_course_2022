6e080c54f5526a439ee00ebbcb7b1139a233a34b
DenisRustand <44909963+DenisRustand@users.noreply.github.com>
2022-05-27 15:55:55 +0200

    Add slides and codes for joint models

--- INLA_joint_example.R
+++ INLA_joint_example.R
@@ -48,8 +48,45 @@ summary(lon)
 
 
 
+NL <- dim(lon)[1]
+NS <- dim(surv)[1]
+
+data <- list(
+  IntL1 = c(rep(1, NL), rep(NA, NS)),
+  TimeL1 = c(lon$time, rep(NA, NS)),
+  IntS1 = c(rep(NA, NL), rep(1, NS)),
+  b_i0L1 = c(lon$id, rep(NA, NS)),
+  b_i0S1 = c(rep(NA, NL), surv$id),
+  Yjoint = list(L1 = c(lon$L1, rep(NA, NS)),
+                S1 = inla.surv(time = c(rep(NA, NL), surv$deathTimes), 
+                               event = c(rep(NA, NL), surv$d)))
+)
+formula <- Yjoint ~ -1 + IntL1 + TimeL1 + IntS1 + 
+  f(b_i0L1, model="iid") + f(b_i0S1, copy="b_i0L1", fixed=FALSE)
+
+inla.setOption(inla.mode="experimental")
+JointM <- inla(formula = formula, data = data, 
+               family=c("gaussian", "exponentialsurv"),
+               control.inla = list(int.strategy="eb"))
+summary(JointM)
+
+lambda <- inla.tmarginal(function(x) exp(x), JointM$marginals.fixed$IntS1)
+inla.zmarginal(lambda)
+ResErr <- inla.tmarginal(function(x) sqrt(1/x), 
+                         JointM$marginals.hyperpar$`Precision for the Gaussian observations`)
+inla.zmarginal(ResErr)
+SD_b <- inla.tmarginal(function(x) sqrt(1/x), 
+                       JointM$marginals.hyperpar$`Precision for b_i0L1`)
+inla.zmarginal(SD_b)
 
 
+library(INLAjoint)
+S1 <- inla.surv(time = surv$deathTimes, event = surv$d)
+JointM_2 <- joint(formLong = L1 ~ time + (1|id),
+                  formSurv = S1 ~ 1, basRisk = "rw1",
+                  id="id", timeVar = "time", assoc = "SRE",
+                  dataLong = lon, dataSurv = surv)
+summary(JointM_2, sdcor=TRUE)
 
 
 
